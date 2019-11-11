7f39415b8cf119326db6165aab7f83ae10d4ae99
yun63 <yun63@126.com>
2019-11-11 19:57:57 +0800

    init-elpa

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -19,7 +19,7 @@
                     (not (gnutls-available-p))))
        (proto (if no-ssl "http" "https")))
   (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  (add-to-list 'package-acrhives (cons "melpa-stable" (concat proto  "://stable.melpa.org/packages/")) t)
+  (add-to-list 'package-archives (cons "melpa-stable" (concat proto  "://stable.melpa.org/packages/")) t)
   ;; Official MELPA Mirror, in case necessary.
   ;;(add-to-list 'package-archives (cons "melpa-mirror" (concat proto "://www.mirrorservice.org/sites/melpa.org/packages/")) t)
   )
