73d6ed34f79df97ce204c0b35a46c427c0bcedd3
yun63 <yun63@126.com>
2019-11-11 21:03:26 +0800

    add some plugin

--- init.el
+++ init.el
@@ -69,9 +69,9 @@
 ;;;
 ;;;(require 'init-recentf)
 (require 'init-smex)
-;;;(require 'init-ivy)
-;;;;;(require 'init-helm)
-;;;(require 'init-hippie-expand)
+(require 'init-ivy)
+;;;(require 'init-helm)
+(require 'init-hippie-expand)
 (require 'init-company)
 ;;;(require 'init-windows)
 ;;;(require 'init-sessions)
