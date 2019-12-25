2ebe275a591677d5b2166b46788d7575811ad029
yun63 <towardstheway@gmail.com>
2019-12-26 00:04:57 +0800

    company

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -13,15 +13,17 @@
   :hook (after-init . global-company-mode)
   :config
   (setq company-idle-delay 0)
+  (setq company-show-numbers t)
   (setq company-minimum-prefix-length 3)
   (setq-default company-dabbrev-other-buffers 'all
 		company-tooltip-align-annotations t)
   (define-key company-active-map (kbd "C-n") 'company-select-next)
   (define-key company-active-map (kbd "C-p") 'company-select-previous)
-  (use-package company-quickhelp
-    :ensure t
-    :init (add-hook 'global-company-mode #'company-quickhelp-mode))
   )
+
+(use-package company-quickhelp
+  :ensure t
+  :init (add-hook 'global-company-mode #'company-quickhelp-mode))
 
 (use-package smex
   :ensure t
