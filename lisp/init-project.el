62d76b4ab57e139028d72f042239b73276a594d8
yun63 <towardstheway@gmail.com>
2019-12-31 00:36:02 +0800

    c++ irony

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -4,12 +4,30 @@
 
 (use-package irony
   :ensure t
-  :init
-  (add-hook 'c++-mode-hook 'irony-mode)
-  (add-hook 'c-mode-hook 'irony-mode)
-  (add-hook 'objc-mode-hook 'irony-mode)
-  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+  :config
+  (progn
+    (unless (irony--find-server-executable) (call-interactively #'irony-install-server))
+    (add-hook 'c++-mode-hook 'irony-mode)
+    (add-hook 'c-mode-hook 'irony-mode)
+    (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))
   )
+
+(use-package company-c-headers
+  :ensure t)
+
+(use-package company-irony
+  :requires (company irony)
+  :config
+  (progn
+    (eval-after-load 'company '(add-to-list 'company-backends 'company-irony))))
+
+(use-package flycheck-irony
+  :requires (flycheck irony)
+  :hook (flycheck-mode . flycheck-irony-setup))
+
+(use-package irony-eldoc
+  :requires (eldoc irony)
+  :hook (irony-mode . irony-eldoc))
 
 ;; company-c-headers
 (use-package company-c-headers
