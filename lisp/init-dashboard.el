ae98b0a52459c07cbc5b91a57ec64a5b48dd6726
yun63 <towardstheway@gmail.com>
2020-01-11 20:56:14 +0800

    remove dashboard

--- lisp/init-dashboard.el
+++
@@ -1,9 +0,0 @@
-;;; init-dashboard.el --- -*- lexical-binding: t -*-
-;;; Commentary:
-;;; Code:
-
-(require-package 'dashboard)
-(dashboard-setup-startup-hook)
-(provide 'init-dashboard)
-
-;;; init-dashboard.el ends here
