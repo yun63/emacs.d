ec84a450a12a5f56bde5953f4a1230a3bbcd9ea1
yun63 <towardstheway@gmail.com>
2020-03-01 01:11:18 +0800

    remove ace-jump-mode

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -4,17 +4,11 @@
 
 
 (use-package evil
-  :ensure t
-  :defer 1
   :init
   (evil-mode 1))
 
 (use-package evil-leader
   :ensure t)
-
-(use-package ace-jump-mode
-  :ensure t
-  :commands ace-jump-mode)
 
 
 (provide 'init-evil)
