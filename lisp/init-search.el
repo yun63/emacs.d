8a7b0cb5bca0e06a52821c343705b1f958acb252
yun63 <towardstheway@gmail.com>
2021-04-26 01:24:53 +0800

    add search

--- lisp/init-search.el
+++ lisp/init-search.el
@@ -15,8 +15,10 @@
 
 (use-package helm-ag
   :config
-  (setq helm-ag-base-command "rg --line-number --no-heading --smart-case")
+  (setq helm-ag-base-command "rg --line-number --no-heading --smart-case --colors=line:fg:yellow --colors=line:style:bold --colors=path:fg:green --colors=path:style:bold --colors=match:fg:black --colors=match:bg:yellow --colors=match:style:nobold")
   (setq helm-ag-fuzzy-match t)
+  (setq helm-follow-mode-persistent t)
+  (setq helm-ag-insert-at-point 'symbol)
   (setq helm-ag-use-agignore t))
 
 (provide 'init-search)
